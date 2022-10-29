a5830570988a20e366e5b52f5e25cdc1f43e5f09
Radit10 <85211622+Radit10@users.noreply.github.com>
2022-10-29 16:16:51 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -25,7 +25,7 @@ global.available = false //status online (online)
 global.unavailable = false //status offline (offline)
 global.pause = false //YNTKTS
 global.autovoice = false //prank autovoice (auto voice pas ada yg toxic) //punya gw
-global.autobio = true //auto set bio (kalau ada orang yg komen)
+global.autobio = false //auto set bio (kalau ada orang yg komen)
 global.autoreadsw = true //auto read SW (story WhatsApp)
 global.publik = true //publik mode
 
