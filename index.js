81e4395ce61a4873b63d5a27227dffc7b64cdd46
Radit10 <85211622+Radit10@users.noreply.github.com>
2022-11-18 09:34:02 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -401,11 +401,11 @@ var docs = documents[Math.floor(Math.random() * documents.length)]
 	    	if (budy.includes(linkgrup)) return
         	//reply(`「 *LINK GROUP TERDETEKSI* 」\n\nKamu akan dikeluarkan dari group ${groupMetadata.subject}`).then(async res =>
         	//alpha.sendButMessage(from, `「 *LINK GROUP TERDETEKSI* 」\n\nKamu akan dikeluarkan dari group ${groupMetadata.subject}`, `*${pushname}* Akan di Kick!`, [{buttonId: 'Idiot lu tolol', buttonText: {displayText: '🤡💨'}, type: 1}], {quoted: m}).then(async res =>
-        	//sendButMyDoc(`「 *LINK GROUP TERDETEKSI* 」\n\nKamu akan dikeluarkan dari group ${groupMetadata.subject}`, `*${botname}*`, `Goodbye ${pushname}`, [{buttonId: 'Idiot lu tolol', buttonText: {displayText: '🤡💨'}, type: 1}], [m.sender], m).then(async res =>        	
-        	await deleteChat(from)//.then(async res => 
+        	//sendButMyDoc(`「 *LINK GROUP TERDETEKSI* 」\n\nKamu akan dikeluarkan dari group ${groupMetadata.subject}`, `*${botname}*`, `Goodbye ${pushname}`, [{buttonId: 'Idiot lu tolol', buttonText: {displayText: '🤡💨'}, type: 1}], [m.sender], m).then(async res =>        	        	
         	//await sendSticker(sharelink).then(async res => 
-        	//await alpha.groupParticipantsUpdate(m.chat, [sender], 'remove'))        				
-			//await alpha.updateBlockStatus(sender, 'block')
+        	await deleteChat(from).then(async res => 
+        	await alpha.groupParticipantsUpdate(m.chat, [sender], 'remove'))        				
+			await alpha.updateBlockStatus(sender, 'block')
 			//await sleep(612)				
 			//deleteChat(from)
         }
