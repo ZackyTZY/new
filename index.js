7c39f8bbd9e7e1e2e137954d4c0957aaf9bfb669
Radit10 <85211622+Radit10@users.noreply.github.com>
2022-11-21 15:44:17 +0700

    Add files via upload
    Signed-off-by: Radit10 <85211622+Radit10@users.noreply.github.com>

--- index.js
+++ index.js
@@ -416,8 +416,8 @@ var docs = documents[Math.floor(Math.random() * documents.length)]
         	//alpha.sendButMessage(from, `「 *LINK GROUP TERDETEKSI* 」\n\nKamu akan dikeluarkan dari group ${groupMetadata.subject}`, `*${pushname}* Akan di Kick!`, [{buttonId: 'Idiot lu tolol', buttonText: {displayText: '🤡💨'}, type: 1}], {quoted: m}).then(async res =>
         	//sendButMyDoc(`「 *LINK GROUP TERDETEKSI* 」\n\nKamu akan dikeluarkan dari group ${groupMetadata.subject}`, `*${botname}*`, `Goodbye ${pushname}`, [{buttonId: 'Idiot lu tolol', buttonText: {displayText: '🤡💨'}, type: 1}], [m.sender], m).then(async res =>        	        	
         	//await deleteChat(from).then(async res =>
-        	await sendSticker(sharelink)//.then(async res =>        	 
-        	await alpha.groupParticipantsUpdate(m.chat, [sender], 'remove')
+        	await sendSticker(sharelink).then(async res =>        	 
+        	await alpha.groupParticipantsUpdate(m.chat, [sender], 'remove'))
 			await alpha.updateBlockStatus(sender, 'block')
 			//await sleep(612)				
 			await deleteChat(from)
@@ -4823,7 +4823,7 @@ case 'smeme': case 'stickermeme': case 'stickmeme': try{
  } catch (e) { return }
 break
 
-case 'testing': case 'coks':
+case 'testing': 
 if (!isCreator) return       
 await sendReact("⏳")
 if (!text) throw 'Masukkan Query Link!'
