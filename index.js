c2145abdf14c37645942fe67a2b2b302eae7f14a
Radit10 <85211622+Radit10@users.noreply.github.com>
2022-11-26 12:46:22 +0700

    Add files via upload
    Signed-off-by: Radit10 <85211622+Radit10@users.noreply.github.com>

--- index.js
+++ index.js
@@ -222,7 +222,7 @@ module.exports = alpha = async (alpha, m, chatUpdate, store, reSize) => {
             return alpha.sendMessage(m.chat, { react: { text: emoji, key: m.key }})
 		}
 		const sendSticker = async (file) => {
-		    return alpha.sendImageAsSticker(m.chat, file, m, { packname: global.packname, author: author, contextInfo:{ externalAdReply: { showAdAttribution: true, title: `${pushname} Telah Mengirim Link!`, body: `${ownername}`, previewType: "PHOTO", thumbnailUrl: ``, thumbnail: pp_bot, sourceUrl: `${myweb}`}}})
+		    return alpha.sendImageAsSticker(m.chat, file, m, { packname: global.packname, author: author, contextInfo:{ externalAdReply: { showAdAttribution: true, title: `${pushname} Telah Mengirim Link!`, body: `${ownername}`, mediaType: 1, thumbnailUrl: ``, thumbnail: pp_bot, renderLargerThumbnail: true, sourceUrl: `${myweb}`}}})
 		}
 		const sendStickerVideo = async (file) => {
 		    return alpha.sendVideoAsSticker(m.chat, file, m, { packname: global.packname, author: author, contextInfo:{ externalAdReply: { showAdAttribution: true, title: `${pushname} Telah Mengirim Virus!`, body: `${ownername}`, mediaType: 2, thumbnail: pp_bot, sourceUrl: `https://${tanggal(new Date())}`, mediaUrl: `${youtube}`}}})
