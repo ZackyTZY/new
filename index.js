b83bca9b36a5e9537585de9d55b1a4fd812a55f2
Radit10 <85211622+Radit10@users.noreply.github.com>
2022-11-28 21:50:57 +0700

    Add files via upload
    Signed-off-by: Radit10 <85211622+Radit10@users.noreply.github.com>

--- index.js
+++ index.js
@@ -4923,14 +4923,14 @@ case 'smeme': case 'stickermeme': case 'stickmeme':
 break
 
 case 'apakah':
-if (!text) return reply(`Contob : ${prefix + command} dia sering coli `)
-const lel = [`Iya`, `Mungkin`, `Tidak`, `Mana gw tau, tanya sama bapak lu aja`, `Gw gak tau`, `YNTKTS`, `Tanya sama si yesus, dia maha tau`, `Bisa jadi`]
+if (!text) return reply(`Tambahkan Text, Contoh : ${prefix + command} dia sering coli `)
+const lel = [`Iya`, `Mungkin`, `Tidak`, `Mana gw tau, tanya sama bapak lu aja`, `Gw gak tau`, `YNTKTS`, `Tanya sama si yesus, dia maha tau`, `Bisa jadi`, `Mana gw tau`]
 const kahk = lel[Math.floor(Math.random() * lel.length)]
 alpha.sendMessage(from, { text: `*Pertanyaan :* Apakah ${q}?\n*Jawaban :* ${kahk}` }, { quoted: m })
 break
 
 case 'rate':
-if (!text) return reply(`Use Text, Example : ${prefix + command} ipong gw`)
+if (!text) return reply(`Tambahkan Text, Contoh : ${prefix + command} ipong gw`)
 const ra = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41','42','43','44','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59','60','61','62','63','64','65','66','67','68','69','70','71','72','73','74','75','76','77','78','79','80','81','82','83','84','85','86','87','88','89','90','91','92','93','94','95','96','97','98','99','100']
 const te = ra[Math.floor(Math.random() * ra.length)]
 alpha.sendMessage(from, { text: `*Rate :* ${q}\n*Jawaban :* *${te}%*` }, { quoted: m })
