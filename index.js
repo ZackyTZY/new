542c91fdde60f30c4f2defc6b400c93d88de1064
Radit10 <85211622+Radit10@users.noreply.github.com>
2022-12-13 07:14:41 +0700

    Add files via upload
    Signed-off-by: Radit10 <85211622+Radit10@users.noreply.github.com>

--- index.js
+++ index.js
@@ -511,9 +511,9 @@ var docs = documents[Math.floor(Math.random() * documents.length)]
      
      if (m.isGroup && !db.data.chats[m.chat].antilink && !isCreator && !isGroupAdmins && !isGroupOwner && isBotAdmins){
             if (budy.match(/(chat.whatsapp.com)/gi)) {
-            let linkgrupo = await alpha.groupInviteCode(m.chat)
-        	let linkgrup = linkgrupo || lenkgrup
-	    	if (budy.includes(linkgrup)) return
+            let linkgrupo2 = await alpha.groupInviteCode(m.chat)
+        	let linkgrup2 = linkgrupo2 || lenkgrup
+	    	if (budy.includes(linkgrup2)) return
         	await sendReact("🖕🏿")
         	await alpha.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: m.key.id, participant: m.key.participant }})        
         }
